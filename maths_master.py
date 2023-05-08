8b93edc5e97018273b7e9e8923e0c98e8db69b2c
st19292 <128324391+st19292@users.noreply.github.com>
2023-05-08 18:10:36 +0900

    May 8 2023 - Component 1.2: Headings & Button
    Changed to Custom Tkinter to allow more possibilities and ease when designing the interface relative to the wireframe. One button was added as a test but images are to be considered in the future to make the middle proximity of the operators buttons row.

--- maths_master.py
+++ maths_master.py
@@ -2,29 +2,33 @@
 from tkinter import *
 '''GUI Python Module'''
 
+from customtkinter import *
+'''GUI Module With Extensive Features'''
+
+
 # Creates Window
-maths_master = Tk()
-maths_master.title("Maths Master")
-maths_master.geometry("350x350")
-maths_master.configure(bg="#242526")
+root = CTk()
+root.title("Maths Master")
+appWidth, appHeight = 500, 500
+set_appearance_mode("dark")
+set_default_color_theme("green")
 
-# Main & Subheading
-main_heading = Label(text = "Maths Master", 
-                    font = ("Gill Sans Ultra Bold", "20"), bg = "#242526", fg = "white",
-                    width = "20", height = "2")
-main_heading.pack()
+# Main & Subheading (Row 0-1)
+main_heading = Label(text="Maths Master: Basic Facts Quiz",
+                     font=("Raleway", "20", "bold"), bg="#242526", fg="white",
+                     width="30")
+main_heading.grid(row=0, column=0, pady=(10, 0))
 
-subheading = Label(text = "Test your maths knowledge through choosing a basic facts challenge!", 
-                font = ("Gill Sans MT", "10"), bg = "#242526", fg = "white",
-                width = "80", height = "2")
-subheading.pack()
+subheading = Label(text="Test your maths knowledge through choosing a basic facts challenge!",
+                   font=("Raleway", "10"), bg="#242526", fg="white",
+                   width="80")
+subheading.grid(row=1, column=0, pady=(10, 0))
+
+# Quiz Choice (Row 2-3)
 
 
-
-'''
-main_heading = Label(top_frame, text = "Frame", width = "20", height = "4", font = ("Arial", "20", "bold"))
-main_heading.pack(fill=X)
-'''
+button1 = CTkButton(root, text="Gaming")
+button1.grid()
 
 # Displays GUI
-maths_master.mainloop()
+root.mainloop()
