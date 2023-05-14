10a0d97147f9069cb10099bcd44151af7bdb4c42
st19292 <128324391+st19292@users.noreply.github.com>
2023-05-14 16:21:44 +0900

    14052023 - Component 2.2.4: All Operators
    Finished all the operators equations, where the division one has an extra layer of complexity. This includes the code where the second number is not divisible by itself to prevent easiness regarding prime numbers.

--- subcomponent_question_randomizer.py
+++ subcomponent_question_randomizer.py
@@ -37,3 +37,75 @@ while True:
     # Given Unknown Error, Addresses
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+
+
+"""
+import random
+
+number_one = random.randint(1, 25)
+number_two = random.randint(1, 25)
+
+answer = number_one - number_two
+
+while True:
+    try:
+        user_answer = float(input(f"What is {number_one} - {number_two}: "))
+        if user_answer == answer:
+            print("Correct!")
+            break
+        else:
+            print(f"Incorrect, the answer is {answer}!")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+"""
+
+
+"""
+import random
+
+number_one = random.randint(1, 25)
+number_two = random.randint(1, 25)
+
+answer = number_one * number_two
+
+while True:
+    try:
+        user_answer = float(input(f"What is {number_one} x {number_two}: "))
+        if user_answer == answer:
+            print("Correct!")
+            break
+        else:
+            print(f"Incorrect, the answer is {answer}!")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+"""
+
+
+"""
+import random
+
+number_one = random.randint(1, 25)
+
+# Adds Difficulty & Preventing Simple Prime Numbers Questions
+number_one_divisor = [i for i in range(1, number_one + 1) if number_one % i == 0]
+number_two = random.choice(number_one_divisor)
+
+answer = number_one / number_two
+
+while True:
+    try:
+        user_answer = float(input(f"What is {number_one} ÷ {number_two}: "))
+        if user_answer == answer:
+            print("Correct!")
+            break
+        else:
+            print(f"Incorrect, the answer is {answer}!")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+"""
