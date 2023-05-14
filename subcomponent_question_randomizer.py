0ed3bad50402a64a37e67107cfaa427bd1d399f3
st19292 <128324391+st19292@users.noreply.github.com>
2023-05-14 16:17:20 +0900

    14052023 - Component 2.2.4: Addition Calculations
    By adding a break alongside other exception for the program to catch, I am able to enforce future proofing when there is incorrect inputs. The next step is the creation of the other operators.

--- subcomponent_question_randomizer.py
+++ subcomponent_question_randomizer.py
@@ -1,13 +1,39 @@
+# Project: Maths Master - Quiz Interface Randomizer Subcomponent
+# Component 2.2.4: Finished Quiz Program Draft
+# 14052023: Draft Layout for all Operators
+
+
+# Classes Importing
 import random
+'''Randomizer Module'''
 
-number_one = random.randint(0, 99)
-number_two = random.randint(0, 99)
 
+# Randomizes Two Number from 1 to 25
+number_one = random.randint(1, 25)
+number_two = random.randint(1, 25)
+
+# Answer Set as their Sum
 answer = number_one + number_two
-user_answer = int(input(f"{number_one} + {number_two}"))
 
-if user_answer == answer:
-    print("Correct!")
-else:
-    print(f"Incorrect, the answer is {answer}!")
-    
+
+while True:
+    """Main Loop Input"""
+
+    # Try Except until Correct User Input
+    try:
+        user_answer = float(input(f"What is {number_one} + {number_two}: "))
+
+        # Notes Input as Correct or Incorrect
+        if user_answer == answer:
+            print("Correct!")
+            break
+        else:
+            print(f"Incorrect, the answer is {answer}!")
+
+    # Addresses Invalid Input
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+    # Given Unknown Error, Addresses
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
