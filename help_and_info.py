391087619ec25a5f1df16d25ba2f5bf1bae356e4
st19292 <128324391+st19292@users.noreply.github.com>
2023-05-18 13:39:26 +1200

    18052023 - Component 3.2: Middle Information
    This is the first iteration of the help and info window component, where it will have the main text in the middle that teaches how to use the program and also the functionalities such as ability to export, etc. Currently it is empty in the middle as the text is refined furthermore.

---
+++ help_and_info.py
@@ -0,0 +1,94 @@
+# Project: Maths Master
+# Component 1.3: Final Draft
+# 14052023: Added Classes Execution
+
+
+# Classes Importing
+from tkinter import *
+'''GUI Python Module'''
+
+
+# Maths Master Main Menu
+class MathsMasterApp:
+    def __init__(self):
+        """Initializes Math Master Instance"""
+
+        self.root = Tk()
+        self.root.title("Maths Master: Help & Info")
+        self.root.resizable(False, False)
+        self.root.configure(bg="#242424")
+
+        # Creates Top Proximity Frame
+        self.top_headings_frame = Frame(self.root, padx=10, pady=10, bg="#2B2B2B")
+
+        # Creates Middle Proximity Frame
+        self.middle_operators_frame = Frame(self.root, bg="#242424", padx=10, pady=40)
+
+        # Creates Bottom Proximity Frame
+        self.bottom_buttons_frame = Frame(self.root, bg="#242424", padx=10, pady=20)
+
+        # Calls Frames Methods & Elements
+        self.menu_top_frame()
+        self.menu_middle_frame()
+        self.menu_bottom_frame()
+
+        # Displays GUI
+        self.root.mainloop()
+
+    def menu_top_frame(self):
+        """Top Proximity: Headings Frame Function"""
+
+        # Top Proximity, Header (Row 0)
+        self.top_headings_frame.grid(row=0, column=0)
+
+        # Main & Subheading Labels
+        menu_heading_text = "Help & Information"
+        main_heading = Label(master=self.top_headings_frame, text=menu_heading_text,
+                             font=("Raleway", "20", "bold"), bg="#2B2B2B", fg="white",
+                             width=30)
+        main_heading.pack(pady=(10, 0))
+
+        menu_subheading_text = "Need help? Have a read on how to use the Math Master GUI."
+        subheading = Label(master=self.top_headings_frame, text=menu_subheading_text,
+                           font=("Raleway", "10"), bg="#2B2B2B", fg="white",
+                           width=80)
+        subheading.pack(pady=(10, 0))
+
+    def menu_middle_frame(self):
+        """Middle Proximity: Button Operators Frame Function"""
+
+        # Middle Proximity, Quiz Choice (Row 1-2)
+        self.middle_operators_frame.grid(row=1, column=0, rowspan=2)
+
+        
+
+    def menu_bottom_frame(self):
+        """Bottom Proximity: Button Navigation Frame Function"""
+
+        # Bottom Proximity, Footer Navigation (Row 3)
+        self.bottom_buttons_frame.grid(row=3, column=0, rowspan=4)
+
+        # Help & Info Button Link
+        def help_and_info_button_clicked():
+            """Testing Button"""
+            print("Help & Info Button clicked!")
+
+        # History & Export Link
+        def history_and_export_button_clicked():
+            """Testing Button"""
+            print("History & Export Button clicked!")
+
+        # Help & Info Button
+        help_and_info_button = Button(master=self.bottom_buttons_frame, text="Help & Info",
+                                      font=("Raleway", "11", "bold"), fg="black", bg="white",
+                                      height=1, width=16, relief="flat", command=help_and_info_button_clicked)
+        help_and_info_button.grid(row=3, column=0, padx=8, pady=10)
+
+        # History & Export Button
+        history_and_export_button = Button(master=self.bottom_buttons_frame, text="History & Export",
+                                           font=("Raleway", "11", "bold"), fg="black", bg="white",
+                                           height=1, width=16, relief="flat", command=history_and_export_button_clicked)
+        history_and_export_button.grid(row=3, column=1, padx=8, pady=10)
+
+
+MathsMasterApp()
