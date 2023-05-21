7749906c96dfe1117a28b8c7bfa400a59f2891be
st19292 <128324391+st19292@users.noreply.github.com>
2023-05-21 13:34:59 +1200

    21052023 - Component 4.1: History & Export Heading
    The first iteration involves just changing the heading to its respective window, where the next steps will be the middle proximities that involve the exporting functionalities.

---
+++ history_and_export.py
@@ -0,0 +1,93 @@
+# Project: Maths Master
+# Component 4.1: Initializing History & Export Window
+# 14052023: Added Heading & Subheading
+
+
+# Classes Importing
+from tkinter import *
+'''GUI Python Module'''
+
+
+# Maths Master Main Menu
+class MathsMasterApp:
+    def __init__(self):
+        """Initializes Math Master Instance"""
+
+        self.root = Tk()
+        self.root.title("Maths Master: History & Export")
+        self.root.resizable(False, False)
+        self.root.configure(bg="#242424")
+
+        # Creates Top Proximity Frame
+        self.top_headings_frame = Frame(self.root, padx=10, pady=10, bg="#2B2B2B")
+
+        # Creates Middle Proximity Frame
+        self.middle_operators_frame = Frame(self.root, bg="#242424", padx=10, pady=40)
+
+        # Creates Bottom Proximity Frame
+        self.bottom_buttons_frame = Frame(self.root, bg="#242424", padx=10, pady=20)
+
+        # Calls Frames Methods & Elements
+        self.menu_top_frame()
+        self.menu_middle_frame()
+        self.menu_bottom_frame()
+
+        # Displays GUI
+        self.root.mainloop()
+
+    def menu_top_frame(self):
+        """Top Proximity: Headings Frame Function"""
+
+        # Top Proximity, Header (Row 0)
+        self.top_headings_frame.grid(row=0, column=0)
+
+        # Main & Subheading Labels
+        menu_heading_text = "History & Export"
+        main_heading = Label(master=self.top_headings_frame, text=menu_heading_text,
+                             font=("Raleway", "20", "bold"), bg="#2B2B2B", fg="white",
+                             width=30)
+        main_heading.pack(pady=(10, 0))
+
+        menu_subheading_text = "Revise your wrong answers. " \
+                               "(When exporting, if file name already exists, it will be replaced.)"
+        subheading = Label(master=self.top_headings_frame, text=menu_subheading_text,
+                           font=("Raleway", "10"), bg="#2B2B2B", fg="white",
+                           width=80)
+        subheading.pack(pady=(10, 0))
+
+    def menu_middle_frame(self):
+        """Middle Proximity: Button Operators Frame Function"""
+
+        # Middle Proximity, Quiz Choice (Row 1-2)
+        self.middle_operators_frame.grid(row=1, column=0, rowspan=2)
+
+    def menu_bottom_frame(self):
+        """Bottom Proximity: Button Navigation Frame Function"""
+
+        # Bottom Proximity, Footer Navigation (Row 3)
+        self.bottom_buttons_frame.grid(row=3, column=0, rowspan=4)
+
+        # Return To Menu Button Link
+        def return_to_menu_button_clicked():
+            """Testing Button"""
+            print("Return To Menu Button clicked!")
+
+        # History & Export Link
+        def export_button_clicked():
+            """Testing Button"""
+            print("Export Button clicked!")
+
+        # Return To Menu Button
+        return_to_menu_button = Button(master=self.bottom_buttons_frame, text="Return To Menu",
+                                       font=("Raleway", "11", "bold"), fg="black", bg="white",
+                                       height=1, width=16, relief="flat", command=return_to_menu_button_clicked)
+        return_to_menu_button.grid(row=3, column=0, padx=8, pady=10)
+
+        # Export Button
+        export_button = Button(master=self.bottom_buttons_frame, text="Help & Info",
+                               font=("Raleway", "11", "bold"), fg="black", bg="white",
+                               height=1, width=16, relief="flat", command=export_button_clicked)
+        export_button.grid(row=3, column=1, padx=8, pady=10)
+
+
+MathsMasterApp()
